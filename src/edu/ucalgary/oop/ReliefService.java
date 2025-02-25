052bb96c3f1c16826b45c7013986dc5c43e2c654
aditi-j25 <aditi.jain1@ucalgary.ca>
2025-02-25 12:37:56 -0700

    change

--- src/edu/ucalgary/oop/ReliefService.java
+++ src/edu/ucalgary/oop/ReliefService.java
@@ -3,5 +3,59 @@ package edu.ucalgary.oop;
 public class ReliefService {
     private Inquirer inquirer;
     private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;  
+
+    public void ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        setInquirer(inquirer);
+        setDisasterVictim(missingPerson);
+        setDateOfInquiry(dateOfInquiry);
+        setInfoProvided(infoProvided);
+        setLastKnownLocation(lastKnownLocation);
+    }
+
+    public Inquirer getInquirer(){
+        return inquirer;
+    }
+
+    public void setInquirer(Inquirer inquirer){
+        this.inquirer = inquirer;
+    }
+
+    public DisasterVictim getDisasterVictim(){
+        return missingPerson;
+    }
+
+    public void setDisasterVictim(DisasterVictim missingPerson){
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry(){
+        return dateOfInquiry;
+    }
     
+    public void setDateOfInquiry(String dateOfInquiry) {
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided(){
+        return infoProvided;
+    }
+
+    public void setInfoProvided(String infoProvided){
+        this.infoProvided = infoProvided;
+    }
+
+    public Location getLastKnownLocation(){
+        return lastKnownLocation;
+    }
+
+    public void getLastKnownLocation(Location lastKnownLocation ){
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    boolean isValidDateFormat(String date)
+
+    String getLogDetails()   
 }
